651bf3c650f93c907c612de3461ea8a4d77a92ae
Sean Wines <sean.wines@vuria.com>
2017-09-21 08:42:06 -0700

    Created general layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,40 @@
 $(function () {
+  var aboutSection = $('#about').position().top,
+      commuterSection = $('#commuter-series').position().top - 300,
+      homeSection = $('#home-series').position().top - 300,
+      photoSection = $('#photo').position().top - 200;
+      blogSection = $('#blog').position().top - 200;
+
+  $(window).on('scroll', function () {
+    var position = $(document).scrollTop();
+
+    if (position < 50) {
+      console.log('asdf');
+      $('#side-nav li').removeClass('selected');
+    }
+
+    if (position < aboutSection && position > 200) {
+      console.log('sfa');
+      $('.about-nav').addClass('selected').siblings().removeClass('selected');
+    }
+
+    if (position > commuterSection) {
+      $('.commuter-nav').addClass('selected').siblings().removeClass('selected');
+    }
+
+    if (position > homeSection) {
+      $('.home-nav').addClass('selected').siblings().removeClass('selected');
+    }
+
+    if (position > photoSection) {
+      $('.photo-nav').addClass('selected').siblings().removeClass('selected');
+    }
+
+    if (position > blogSection) {
+      $('.blog-nav').addClass('selected').siblings().removeClass('selected');
+    }
+  })
+
   // Scroll to area
   $(document).on('click', 'a', function(event){
     event.preventDefault();
@@ -19,4 +55,9 @@ $(function () {
     }, 500);
     $('#side-nav li').removeClass('selected');
   })
+
+  // Hide tooltip
+  setTimeout(function () {
+    $('.tooltip').css('bottom', '-10rem')
+  }, 10000)
 })
